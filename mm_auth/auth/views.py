dcf214a3b03a0defd80dcb7c6f60d07567ee6a5f
nathanmandell99@gmail.com <nathanmandell99@gmail.com>
2024-10-01 15:59:53 -0400

    Initial Django setup.

---
+++ mm_auth/auth/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import User
+
+# Create your views here.
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def register(request):
+    pass
