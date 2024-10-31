5db355973168d188a8c04f51e2ae9fae69d1573b
Nathan <nathanmandell99@gmail.com>
2024-10-31 18:56:51 -0400

    Refactor for HATEOAS

--- mm_authservice/authservice/serializers.py
+++ mm_authservice/authservice/serializers.py
@@ -1,15 +1,18 @@
-
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.reverse import reverse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    links = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'password', 'email', 'links']
 
     def create(self, validated_data):
         user = User(**validated_data)
@@ -17,7 +20,57 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    def get_links(self, obj):
+        return [
+            {
+                'rel': 'self',
+                'href': reverse('register', request=self.context['request']),
+            },
+            {
+                'rel': 'token',
+                'href': reverse('token_obtain_pair', request=self.context['request']),
+            },
+        ]
+
 
 class UserTokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+    links = serializers.SerializerMethodField()
+
+    def get_links(self, obj):
+        return [
+            {
+                'rel': 'self',
+                'href': reverse('token_obtain_pair', request=self.context['request']),
+            },
+            {
+                'rel': 'register',
+                'href': reverse('register', request=self.context['request']),
+            },
+        ]
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims here, if needed
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['links'] = self.get_links()
+        return data
+
+    def get_links(self):
+        return [
+            {
+                'rel': 'self',
+                'href': reverse('token_obtain_pair', request=self.context['request']),
+            },
+            {
+                'rel': 'register',
+                'href': reverse('register', request=self.context['request']),
+            },
+        ]
--- mm_authservice/authservice/views.py
+++ mm_authservice/authservice/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import User
+from django.contrib.auth.models import User
 
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserRegisterSerializer, UserTokenSerializer
-
-# Create your views here.
+from .serializers import UserRegisterSerializer, UserTokenSerializer, CustomTokenObtainPairSerializer
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -16,6 +14,16 @@ class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response.data['links'] = response.data.pop('links')
+        return response
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = UserTokenSerializer
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        response.data['links'] = response.data.pop('links')
+        return response
