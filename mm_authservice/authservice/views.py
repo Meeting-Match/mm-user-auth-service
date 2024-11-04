8fd31bc8f66f0563658c20ee07af2c36483b1100
Nathan <nathanmandell99@gmail.com>
2024-11-04 16:38:14 -0500

    Remove superfluous serializer import

--- mm_authservice/authservice/views.py
+++ mm_authservice/authservice/views.py
@@ -6,7 +6,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserRegisterSerializer, UserTokenSerializer, CustomTokenObtainPairSerializer
+from .serializers import UserRegisterSerializer, CustomTokenObtainPairSerializer
 
 
 class UserRegisterView(generics.CreateAPIView):
